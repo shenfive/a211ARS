2ab54f157b2def367eeeab2e2890825206248a08
Shen Jun Wu <shenfive@gmail.com>
2021-01-16 16:56:58 +0800

    plane

--- a211ARS/ViewController.swift
+++ a211ARS/ViewController.swift
@@ -30,22 +30,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         
         sceneView.scene = scene
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
-        let material = SCNMaterial() //新增材質
-        material.diffuse.contents = UIColor.red //材質內容為紅色
-        box.materials = [material] //把 box 的貼圖材質加進去
-        let node = SCNNode(geometry: box) //新增一個 Box
-        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
-        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
+//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
+//        let material = SCNMaterial() //新增材質
+//        material.diffuse.contents = UIColor.red //材質內容為紅色
+//        box.materials = [material] //把 box 的貼圖材質加進去
+//        let node = SCNNode(geometry: box) //新增一個 Box
+//        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
+//        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
         
         
-        let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
-        text.firstMaterial?.diffuse.contents = UIColor.blue
+//        let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
+//        text.firstMaterial?.diffuse.contents = UIColor.blue
         
-        let textNode = SCNNode(geometry: text)
-        textNode.position = SCNVector3(0, 0.05, -0.5)
-        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
-        sceneView.scene.rootNode.addChildNode(textNode)
+//        let textNode = SCNNode(geometry: text)
+//        textNode.position = SCNVector3(0, 0.05, -0.5)
+//        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+//        sceneView.scene.rootNode.addChildNode(textNode)
         
         
         
@@ -53,11 +53,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.addGestureRecognizer(gesture)
         
         
-        let earth = SCNSphere(radius: 0.3)
-        earth.firstMaterial?.diffuse.contents = UIImage(named: "worldmap")
-        let earthNode = SCNNode(geometry: earth)
-        earthNode.position = SCNVector3(0, 0, -1)
-        sceneView.scene.rootNode.addChildNode(earthNode)
+//        let earth = SCNSphere(radius: 0.3)
+//        earth.firstMaterial?.diffuse.contents = UIImage(named: "worldmap")
+//        let earthNode = SCNNode(geometry: earth)
+//        earthNode.position = SCNVector3(0, 0, -1)
+//        sceneView.scene.rootNode.addChildNode(earthNode)
         
         
         
@@ -67,24 +67,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     
     @objc func taped(sender:UIGestureRecognizer){
-        let view = sender.view as! SCNView //由傳送者取得 ARView 的實體
-        let location = sender.location(in: view) //取得點選的畫面座標
-        let hitResult = view.hitTest(location, options: nil) //試試看能不能點到東西
-        if hitResult.isEmpty != true{
-            print("some thing!")
-            let randomColor = UIColor(
-                red: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                green: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                blue:  CGFloat(arc4random()) / CGFloat(UInt32.max),
-                alpha: 1.0)
-            hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
-            
-            
-            
-            
-        }else{
-            print("nothing!")
+        let view = sender.view as! ARSCNView
+        //由傳送者取得 ARView 的實體, 必需為 ARSCNView 才能偵測 plane
+        let location = sender.location(in: view)
+        let hitResult = view.hitTest(location, types: .existingPlaneUsingExtent) //試試是否是點到 plane
+        if let firstHitResults = hitResult.first{
+            self.addSphere(hitResult: firstHitResults)
         }
+        
+        
+        
+        
+//        let view = sender.view as! SCNView //由傳送者取得 ARView 的實體
+//        let location = sender.location(in: view) //取得點選的畫面座標
+//        let hitResult = view.hitTest(location, options: nil) //試試看能不能點到東西
+//        if hitResult.isEmpty != true{
+//            print("some thing!")
+//            let randomColor = UIColor(
+//                red: CGFloat(arc4random()) / CGFloat(UInt32.max),
+//                green: CGFloat(arc4random()) / CGFloat(UInt32.max),
+//                blue:  CGFloat(arc4random()) / CGFloat(UInt32.max),
+//                alpha: 1.0)
+//            hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
+//
+//        }else{
+//            print("nothing!")
+//        }
+    }
+    @objc func addSphere(hitResult:ARHitTestResult){
+        let sphere = SCNSphere(radius: 0.025)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "worldmap")
+        sphere.materials = [material]
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y+0.5, hitResult.worldTransform.columns.3.z)
+        sphereNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)//加上物理特性並啟動
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
     }
 
     
